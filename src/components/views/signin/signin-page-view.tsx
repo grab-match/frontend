6af4f9cc0fd5b1635b91cfccef3a96078166354c
hary <haryarrahman14@gmail.com>
2024-07-27 14:56:56 +0700

    feat : login ui

--- src/components/views/signin/signin-page-view.tsx
+++ src/components/views/signin/signin-page-view.tsx
@@ -1,5 +1,8 @@
 import { auth, signIn } from "@/auth";
+import Image from "next/image";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export function SignInPageView() {
   return (
@@ -11,16 +14,47 @@ export function SignInPageView() {
         });
       }}
     >
-      <Button type="submit">Signin with Google</Button>
-      <Button
-        onClick={async () => {
-          "use server";
-          const response = await auth();
-          console.log({ response });
-        }}
-      >
-        Check Auth
-      </Button>
+      <Image
+        alt="Login"
+        src="/images/login.svg"
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
+
+      <div className="mx-auto max-w-[325px]">
+        <p className="text-center text-white font-bold text-[36px] mb-[36px]">
+          GrabMatch
+        </p>
+
+        <div className="flex flex-col gap-[30px]">
+          <Input type="email" placeholder="Email" className="bg-white" />
+          <Input type="password" placeholder="Password" className="bg-white" />
+
+          <div className="flex flex-row gap-[16px]">
+            <Button type="submit" className="bg-emerald-500">
+              Login
+            </Button>
+            <Button
+              className="flex-grow bg-white text-black"
+              onClick={async () => {
+                "use server";
+                const response = await auth();
+                console.log({ response });
+              }}
+            >
+              <Image
+                className="mr-[8px]"
+                alt="google"
+                src="/logos/google.svg"
+                width={24}
+                height={24}
+              />
+              Continue with Google
+            </Button>
+          </div>
+        </div>
+      </div>
     </form>
   );
 }
