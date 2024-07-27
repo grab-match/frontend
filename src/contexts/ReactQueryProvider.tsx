312184bf1fff706e5c3d6cc0746cdd7fe6cd217b
hary <haryarrahman14@gmail.com>
2024-07-27 12:44:18 +0700

    feat : setup react query dev tools

--- src/contexts/ReactQueryProvider.tsx
+++ src/contexts/ReactQueryProvider.tsx
@@ -1,25 +1,60 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+  isServer,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode } from "react";
 
 interface ReactQueryProviderProps {
-  children: JSX.Element | JSX.Element[];
+  children: ReactNode;
 }
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 0,
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: 0,
+      },
     },
-  },
-});
+    queryCache: new QueryCache({
+      onError: (e: unknown) => {
+        console.log("QueryCache error:", e);
+      },
+    }),
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 const ReactQueryProvider: React.FC<ReactQueryProviderProps> = ({
   children,
 }) => {
+  const queryClient = getQueryClient();
+
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+    </QueryClientProvider>
   );
 };
 
