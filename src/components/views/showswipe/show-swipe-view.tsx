cc7e0ab121c235e3e7e2fe2b4864c3cd6a4de7ed
hary <haryarrahman14@gmail.com>
2024-07-28 09:19:16 +0700

    fix : issue

--- src/components/views/showswipe/show-swipe-view.tsx
+++ src/components/views/showswipe/show-swipe-view.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { Icon } from "@iconify/react";
 
 export function ShowSwipePage() {
   const [page, setPage] = useState("YourSet");
@@ -35,22 +36,14 @@ export function ShowSwipePage() {
         </div>
         {page === "YourSet" && (
           <div className="flex bg-[#F7F7F7] rounded-xl pr-3 pl-1 py-0.5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path
-                d="M12 11.5C11.337 11.5 10.7011 11.2366 10.2322 10.7678C9.76339 10.2989 9.5 9.66304 9.5 9C9.5 8.33696 9.76339 7.70107 10.2322 7.23223C10.7011 6.76339 11.337 6.5 12 6.5C12.663 6.5 13.2989 6.76339 13.7678 7.23223C14.2366 7.70107 14.5 8.33696 14.5 9C14.5 9.3283 14.4353 9.65339 14.3097 9.95671C14.1841 10.26 13.9999 10.5356 13.7678 10.7678C13.5356 10.9999 13.26 11.1841 12.9567 11.3097C12.6534 11.4353 12.3283 11.5 12 11.5ZM12 2C10.1435 2 8.36301 2.7375 7.05025 4.05025C5.7375 5.36301 5 7.14348 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 7.14348 18.2625 5.36301 16.9497 4.05025C15.637 2.7375 13.8565 2 12 2Z"
-                fill="#F24E1E"
-              />
-            </svg>
-            <p>Menteng, Jakarta Pusat</p>
+            <Icon
+              icon="fluent:calendar-sync-20-regular"
+              className="h-6 w-6 mr-2"
+            />
+            <span>Sunday, 28 July 2024</span>
           </div>
         )}
-        <p className="font-bold">Lorem Ipsum </p>
+        <p className="font-bold">Basic filter based your Preferences</p>
         <div className="flex gap-1.5">
           <button className="bg-[#F7F7F7] p-1.5  rounded-lg">
             <svg
@@ -129,7 +122,9 @@ export function ShowSwipePage() {
             </div>
             <div className="bg-white rounded text-black p-1.5">
               <p>What are you looking for?</p>
-              <p>singkat saja ayo pacaran</p>
+              <p>
+                <b>singkat saja ayo pacaran</b>
+              </p>
             </div>
           </div>
         </div>
